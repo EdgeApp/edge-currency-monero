56e1442a0724f5d6403519169e0b0ca7135ed76a
Paul Puey <paul@edgesecure.co>
2018-05-31 02:18:44 +0200

    WIP

---
+++ src/currencyInfoXMR.js
@@ -0,0 +1,42 @@
+/* global */
+// @flow
+
+import type { EdgeCurrencyInfo } from 'edge-core-js'
+import type { MoneroSettings } from './xmrTypes.js'
+
+const otherSettings: MoneroSettings = {
+  mymoneroApiServers: [
+    'https://api.mymonero.com:8443'
+  ]
+}
+
+const defaultSettings: any = {
+  otherSettings
+}
+
+export const currencyInfo: EdgeCurrencyInfo = {
+  // Basic currency information:
+  currencyCode: 'XMR',
+  currencyName: 'Monero',
+  pluginName: 'monero',
+  walletTypes: [
+    'wallet:monero'
+  ],
+
+  defaultSettings,
+
+  addressExplorer: 'https://moneroblocks.info/search/%s',
+  transactionExplorer: 'https://moneroblocks.info/tx/%s',
+
+  denominations: [
+    // An array of Objects of the possible denominations for this currency
+    {
+      name: 'XMR',
+      multiplier: '1000000000000',
+      symbol: '‎ɱ'
+    }
+  ],
+  symbolImage: 'https://developer.airbitz.co/content/monero-symbol-64.png', // Base64 encoded png image of the currency symbol (optional)
+  symbolImageDarkMono: 'https://developer.airbitz.co/content/monero-symbol-64-87939D.png', // Base64 encoded png image of the currency symbol (optional)
+  metaTokens: []
+}
---
+++ src/xmrSchema.js
@@ -0,0 +1,134 @@
+// /**
+//  * Created by paul on 8/27/17.
+//  */
+//
+// export const CurrencyInfoSchema = {
+//   'type': 'object',
+//   'properties': {
+//     'walletTypes': {
+//       'type': 'array',
+//       'items': {'type': 'string'}
+//     },
+//     'currencyCode': { 'type': 'string' },
+//     'currencyName': { 'type': 'string' },
+//     'addressExplorer': { 'type': 'string' },
+//     'transactionExplorer': { 'type': 'string' },
+//     'defaultSettings': {
+//       'type': 'object',
+//       'properties': {
+//         'otherSettings': {
+//           'type': 'object',
+//           'properties': {
+//             'etherscanApiServers': {
+//               'type': 'array',
+//               'items': {'type': 'string'}
+//             },
+//             'superethServers': {
+//               'type': 'array',
+//               'items': {'type': 'string'}
+//             }
+//           }
+//         }
+//       }
+//     },
+//     'denominations': {
+//       'type': 'array',
+//       'items': {
+//         'type': 'object',
+//         'properties': {
+//           'name': { 'type': 'string' },
+//           'multiplier': { 'type': 'string' },
+//           'symbol': { 'type': 'string' }
+//         },
+//         'required': [ 'name', 'multiplier' ]
+//       }
+//     },
+//     'symbolImage': { 'type': 'string' },
+//     'metaTokens': {
+//       'type': 'array',
+//       'items': {
+//         'type': 'object',
+//         'properties': {
+//           'currencyCode': { 'type': 'string' },
+//           'currencyName': { 'type': 'string' },
+//           'denominations': {
+//             'type': 'array',
+//             'items': {
+//               'type': 'object',
+//               'properties': {
+//                 'name': { 'type': 'string' },
+//                 'multiplier': { 'type': 'string' },
+//                 'symbol': { 'type': 'string' }
+//               },
+//               'required': [ 'name', 'multiplier' ]
+//             }
+//           },
+//           'contractAddress': { 'type': 'string' },
+//           'symbolImage': { 'type': 'string' }
+//         },
+//         'required': [ 'currencyCode', 'currencyName', 'denominations' ]
+//       }
+//     }
+//   },
+//   'required': [
+//     'walletTypes',
+//     'currencyCode',
+//     'currencyName',
+//     'defaultSettings',
+//     'denominations',
+//     'symbolImage',
+//     'addressExplorer',
+//     'transactionExplorer'
+//   ]
+// }
+//
+// export const NetworkFeesSchema = {
+//   'type': 'object',
+//   'additionalProperties': {
+//     'type': 'object',
+//     'properties': {
+//       'gasLimit': {
+//         'type': 'object',
+//         'properties': {
+//           'regularTransaction': { 'type': 'string' },
+//           'tokenTransaction': { 'type': 'string' }
+//         },
+//         'required': [ 'regularTransaction', 'tokenTransaction' ]
+//       },
+//       'gasPrice': {
+//         'type': 'object',
+//         'properties': {
+//           'lowFee': { 'type': 'string' },
+//           'standardFeeLow': { 'type': 'string' },
+//           'standardFeeHigh': { 'type': 'string' },
+//           'standardFeeLowAmount': { 'type': 'string' },
+//           'standardFeeHighAmount': { 'type': 'string' },
+//           'highFee': { 'type': 'string' }
+//         },
+//         'required': [ 'lowFee', 'standardFeeLow', 'standardFeeHigh', 'standardFeeLowAmount', 'standardFeeHighAmount', 'highFee' ]
+//       }
+//     },
+//     'required': [ 'gasLimit' ]
+//   }
+// }
+//
+// export const EthGasStationSchema = {
+//   'type': 'object',
+//   'properties': {
+//     'safeLow': {'type': 'number'},
+//     'average': {'type': 'number'},
+//     'fastest': {'type': 'number'}
+//   },
+//   'required': ['safeLow', 'average', 'fastest']
+// }
+//
+// export const CustomTokenSchema = {
+//   'type': 'object',
+//   'properties': {
+//     'currencyCode': {'type': 'string'},
+//     'currencyName': {'type': 'string'},
+//     'multiplier': {'type': 'string'},
+//     'contractAddress': {'type': 'string'}
+//   },
+//   'required': ['currencyCode', 'currencyName', 'multiplier', 'contractAddress']
+// }
