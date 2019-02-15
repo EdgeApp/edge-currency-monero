69bc2caad692623806f25f223ee5e3288aaf6940
William Swanson <swansontec@gmail.com>
2019-02-15 11:38:41 -0800

    Create a React Native webpack bundle

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+const babelOptions = {
+  // For debugging, just remove "@babel/preset-env":
+  presets: ['@babel/preset-env', '@babel/preset-flow'],
+  plugins: [['@babel/plugin-transform-for-of', { assumeArray: true }]],
+  cacheDirectory: true
+}
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/xmrIndex.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: { loader: 'babel-loader', options: babelOptions }
+      }
+    ]
+  },
+  output: {
+    filename: 'edge-currency-monero.js',
+    path: path.join(path.resolve(__dirname), 'lib/react-native')
+  },
+  resolve: {
+    aliasFields: ['react-native'],
+    mainFields: ['react-native', 'module', 'main']
+  }
+}
